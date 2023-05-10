f58c36ebf8f1d8042a32ae97dbf53d623e93e2f2
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-10 11:31:55 +0545

    Java First code

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    
+        public static void main(String[] args){
+            //output
+            System.out.print("Hello World");
+            System.out.println("Hy world");
+            System.out.print("Hey world\n");
+            System.out.println("heyllo world");
+        }
+    }
+    
