a0702642d447868152876564a18cc7b829a754ac
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-11 12:33:57 +0545

    Test: new problem solved

---
+++ ConditionalTest.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+
+
+public class ConditionalTest {
+    public static void main(String[] args) {
+        
+// PROBLEM 1
+// Java Program to Check Leap Year
+
+    // Scanner sc= new Scanner(System.in);
+    // int days;
+    
+    // System.out.println("Enter the date between 1 to 32");
+    // days = sc.nextInt();
+
+    // if(days>=364){
+    //     System.out.println("the year is leap year");
+    // }else{
+    //     System.out.println("the year is not leap year");
+    // }
+
+
+
+// PROBLEM 2
+// Java Program to Check Whether a Number is Positive or Negative
+
+int n1 = 0;     
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter one number");
+n1 = sc.nextInt();
+
+if(n1 >= 0){
+    System.out.println("the number is positive");
+}else{
+    System.out.println("the number is negative");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
