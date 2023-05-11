5dc2ef563d4bae8ac8114d6357b182d12752bbb6
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-11 13:13:25 +0545

    New test added

---
+++ LoopExam.java
@@ -0,0 +1,67 @@
+
+
+public class LoopExam {
+    public static void main(String[] args) {
+
+    
+//---------------------------------------------------------PROBLEM 1----------------------------------------------------------------------------------------------
+// Java Program to Calculate the Sum of Natural Numbers
+// .............................USING FOR LOOP..........................................
+
+// int sum = 0;
+// for(int i = 1; i <=100; i++){
+//     sum = sum + i;    
+// }
+// System.out.println("The total sum of natural number between 1-100 is : " + sum);
+
+
+//................................ USING WHILE LOOP............................................
+
+// int sum = 0;
+// int i= 1;
+// while(i<=100){
+//     sum = sum + i;
+//     i++;
+// }
+// System.out.println("sum = " + sum);
+
+// ...............................USING DO-WHILE LOOP...........................................
+// int i = 1, sum = 0;
+// do{
+    
+//     sum = sum + i;
+//     System.out.println("The sum is : " + sum);
+//     i++;
+// }while(i<=10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
