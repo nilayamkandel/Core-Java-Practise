3bfdb83ee3dcb6d5a9c33b65c3e8835a7b4d4ad5
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-11 11:19:02 +0545

    refactor

---
+++ Input.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Input {
+    
+    public static void main(String[] args){
+        String name;
+        System.out.println("what is your name");
+        Scanner sc = new Scanner(System.in);   
+        name = sc.next();
+        System.out.println("my name is : "  + name);
+                
+    }
+}
