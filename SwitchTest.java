da89daab0c6cd1f2e5cb2d3983da755ecbffc226
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-11 12:09:28 +0545

    Uppercase changed to Lowecase

--- SwitchTest.java
+++ SwitchTest.java
@@ -24,9 +24,10 @@ public class SwitchTest {
      * }
      */
 
+
+
 //  PROBLEM 1
 //  calculate the days of the week
-
 // int day = 3;
 // switch (day) {
 //     case 1: System.out.println("sunday"); break;
@@ -39,6 +40,8 @@ public class SwitchTest {
 //     default: System.out.println("Enter number from 1 to 7");
 // }
 
+
+
 // PROBLEM 2
 /*
 		 * calculate the following post:
@@ -49,49 +52,87 @@ public class SwitchTest {
 		 * MANAGER	18700			17.88%	?
 		 * HELPER	12789			11%		?
 		 */
+// String post = "";
+// double basicSalary, bonus;
+// double totalIncome;
 
-String post = "";
-double basicSalary, bonus;
-double totalIncome;
+// Scanner sc = new Scanner(System.in);
+
+// System.out.println("Enter either one of the post : MD, CEO, MANAGER, HELPER");
+// post = sc.next();
+
+// switch(post){
+//     case "MD": 
+//     basicSalary = 35900;
+//     bonus = 20.67; 
+//     totalIncome =  (basicSalary + (20.67 * 35900)/100);
+//     System.out.println("the total salary is : " + totalIncome);
+//     break;
+
+//     case "CEO":
+//     basicSalary = 23400;
+//     bonus = 18;
+//     totalIncome = (basicSalary + (18*23400)/100);
+//     System.out.println("the total salary is : " + totalIncome);
+//     break;
+
+//     case "MANAGER":
+//     basicSalary = 23400;
+//     bonus = 18;
+//     totalIncome = (basicSalary + (18*23400)/100);
+//     System.out.println("the total salary is : " + totalIncome);
+//     break;
+
+//     case "HELPER":
+//     basicSalary = 23400;
+//     bonus = 18;
+//     totalIncome = (basicSalary + (18*23400)/100);
+//     System.out.println("the total salary is : " + totalIncome);
+//     break;
+
+//     default: System.out.println("no such post");
+// }
+
+
+// PROBLEM 3
+// Calculate add/sub/mul/div (+,-,*,/) of two integer value given by user using switch
+
+int n1=0;
+int n2 = 0;
+String operator;
 
 Scanner sc = new Scanner(System.in);
 
-System.out.println("Enter either one of the post : MD, CEO, MANAGER, HELPER");
-post = sc.next();
+System.out.println("Enter one number n1");
+n1 = sc.nextInt();
 
-switch(post){
-    case "MD": 
-    basicSalary = 35900;
-    bonus = 20.67; 
-    totalIncome =  (basicSalary + (20.67 * 35900)/100);
-    System.out.println("the total salary is : " + totalIncome);
+System.out.println("Enter second number n2");
+n2 = sc.nextInt();
+
+System.out.println("Enter one operator amone add, sub, mul, div");
+operator = sc.next();
+
+switch(operator.toLowerCase()){
+    case "add":
+    System.out.println("the value of sum is : " + (n1 + n2));
     break;
 
-    case "CEO":
-    basicSalary = 23400;
-    bonus = 18;
-    totalIncome = (basicSalary + (18*23400)/100);
-    System.out.println("the total salary is : " + totalIncome);
+    case "sub":
+    System.out.println("the value of sum is : " + (n1 - n2));
     break;
 
-    case "MANAGER":
-    basicSalary = 23400;
-    bonus = 18;
-    totalIncome = (basicSalary + (18*23400)/100);
-    System.out.println("the total salary is : " + totalIncome);
+    case "mul":
+    System.out.println("the value of mul is : " + (n1*n2));
     break;
 
-    case "HELPER":
-    basicSalary = 23400;
-    bonus = 18;
-    totalIncome = (basicSalary + (18*23400)/100);
-    System.out.println("the total salary is : " + totalIncome);
+    case "div":
+    System.out.println("the value of div is : " + (n1/n2));
     break;
 
-    default: System.out.println("no such post");
-
-
+    default: 
+    System.out.println("no such operator");
 }
+
 
 
 
