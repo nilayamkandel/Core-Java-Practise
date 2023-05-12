41efc9b2ee0e08eab0329ba2f3977f8fddee0f61
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-12 13:07:22 +0545

    even number added using for loop

---
+++ ForLoop.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        
+// -------------------------------------------PROBLEM 1--------------------------------------------------------------------------------------
+// Find the  prime number
+
+// int num;
+// Scanner sc = new Scanner(System.in);
+// System.out.println("Enter one value");
+// num = sc.nextInt();
+// boolean prime = true;		
+// 	    if(num == 0 || num == 1) {	
+// 	    	System.out.println(num+" is not a prime number.");
+// 	    }
+	    
+// 	    else {
+// 	    	for (int i = 2; i <= num / 2; ++i) { 	 	
+// 	    	    if (num % i == 0) {
+// 	    		System.out.println(num + " is not a prime number.");
+// 	    		prime= false; 		 
+// 	    		break;
+// 	    		}
+// 	    	}
+// 		   if(prime == true){
+// 		      System.out.println(num + " is a prime number.");
+// 		   	}
+// 	    }
+
+
+
+
+// ==========================================   PROBLEM 2============================================================================
+// Take input from user and add only even number 
+
+int n1, n2, sumOfEven = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter one number (n1) : ");
+		n1 = sc.nextInt();
+		System.out.println("Enter one number (n2) : ");
+		n2 = sc.nextInt();
+		for(int i = n1; i <= n2; i++) {
+			
+			if(i % 2==0) {
+				sumOfEven = i + sumOfEven;
+				
+				}		
+		}System.out.println("the sum of even numbers are : " + sumOfEven);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	           
+	}	
+
+}
+
