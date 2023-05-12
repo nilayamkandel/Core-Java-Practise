38197524c93f3bf6b6d42710f1c5d1fff4d2b2bc
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-12 13:37:38 +0545

    if, elseif, ladder statement

---
+++ LadderTest.java
@@ -0,0 +1,20 @@
+public class LadderTest {
+    public static void main(String[] args) {
+        
+                    
+                    int age = 56;
+                    if(age >=70) {
+                        System.out.println("old");
+                    }
+                    else if(age >= 45){
+                        System.out.println("Adult");
+                    }
+                    
+                    else if(age >=30) {
+                        System.out.println("young");
+                    }
+                    else {
+                        System.out.println("too young");
+                    }
+                }
+ }
