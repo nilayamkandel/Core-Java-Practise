80779ea0e3531a5b149ccd8af66a07682d1b65be
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-13 18:06:53 +0545

    odd numbers between two integer

---
+++ Methods2.java
@@ -0,0 +1,19 @@
+public class Methods2 {
+    // find the odd numbers between two integers
+    static boolean checkEven(int n){
+        
+        if(n%2 == 0){
+            return false;
+        }   
+    return true;
+    
+}
+public static void main(String[] args) {
+int n1 = 2, n2 = 17;
+for(int i = n1; i<=n2; i++){
+    if(checkEven(i)){
+        System.out.println(i + " ");
+    }
+}   
+}
+}
