d5cc867b81f7f0ef654dab2111a92f70e1bd5a65
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-14 13:35:45 +0545

    result printed without using loop

---
+++ arrayWithoutLoop.java
@@ -0,0 +1,21 @@
+public class arrayWithoutLoop {
+    public static void main(String[] args) {
+// Without using for loop
+      
+        int[] subjectMarks = new int[5];
+        subjectMarks[0] = 45;
+        subjectMarks[1] = 50;
+        subjectMarks[2] = 60;
+        subjectMarks[3] = 65;
+        subjectMarks[4] = 70;
+
+        System.out.println(subjectMarks[0]);
+        System.out.println(subjectMarks[1]);
+        System.out.println(subjectMarks[2]);
+        System.out.println(subjectMarks[3]);
+        System.out.println(subjectMarks[4]);
+
+
+    }
+    
+}
