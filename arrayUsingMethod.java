a2197862ccbdaf517bb5fcbcf541fea930d80c7e
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-15 14:47:16 +0545

    smalles value searched

---
+++ arrayUsingMethod.java
@@ -0,0 +1,135 @@
+public class arrayUsingMethod {
+// Print the number 1-9
+// ============================================PROBLEM 1======================================================================
+
+    // static int num(int value[]){
+    //     for(int i=0; i < value.length; i++){
+    //         System.out.println(value[i]);
+    //     }
+    //     return 1;
+    // }
+    // public static void main(String[] args) {
+    //     int Number[] = {1,2,3,4,6,7,8,9};
+    //     num(Number);
+    // }
+
+
+// ================================================PROBLEM 2=========================================================
+// Create an array of 5 floats and calculate their sum.
+     
+        // static double fNum(double values[]){
+        //     double sum = 0;
+        //     for(double i = 0; i < values.length; i++){
+        //         sum = sum + i;        
+        //     }
+        //     System.out.println("sum : " + sum);
+        //     return sum;
+        // }
+        // public static void main(String[] args) {
+        //     double fnumber[] = {2.3,4.5,67.2,7.6,4.5};
+        //     fNum(fnumber);
+        // }
+
+
+// =====================================================PROBLEM 3=================================================
+// find the small value
+
+        // public static void main(String[] args) {
+        //     int sValue[] = {2,0,1,2,6,7,5,3};
+        //     findSmallValue(sValue);
+        // }
+        // static void findSmallValue(int values[]){
+        //     int sv = values[0];
+        //     for(int x: values){
+        //         if(sv>x){
+        //             sv = x;
+        //         }
+        //     }
+        //     System.out.println("sv : " + sv);       
+        // }
+
+
+// ========================================================PROBLEM 4======================================================
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
