b99e101098bf887bbf4c54abddb5be2fe52808d4
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-16 13:47:50 +0545

    class and object

---
+++ classAndobject/Book.java
@@ -0,0 +1,15 @@
+package classAndobject;
+
+public class Book {
+    int id;
+    String author;
+    String subject;
+
+    void bookDetail(){
+        System.out.println("id : " + id);
+        System.out.println("author : " + author);
+        System.out.println("subject : " + subject);
+    }
+
+
+}
---
+++ classAndobject/mainMethodTest.java
@@ -0,0 +1,13 @@
+package classAndobject;
+
+public class mainMethodTest {
+    public static void main(String[] args) {
+         
+    Book b1 = new Book();
+    b1.id = 902;
+    b1.author = "neelkumar";
+    b1.subject ="science";
+    b1.bookDetail();
+
+    }
+}
