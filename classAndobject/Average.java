c228b9e644e8321fe322f2aae1f27afece233e1c
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-18 11:20:54 +0545

    calculate average

---
+++ classAndobject/Average.java
@@ -0,0 +1,35 @@
+package classAndobject;
+
+import java.util.Scanner;
+
+public class Average {
+    int n1;
+	int n2;
+	int n3;
+	
+	
+	void Calculate() {
+		double avg;
+		avg = (n1 + n2 + n3)/3.0;
+		System.out.println("the average value is : " + avg);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter three number");
+		
+		Average a1 = new Average();
+		
+		a1.n1 = sc.nextInt();
+		a1.n2 = sc.nextInt();
+		a1.n3 = sc.nextInt();
+		
+		a1.Calculate();
+		
+	}
+	
+	
+}
