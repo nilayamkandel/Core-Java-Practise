1a6d5020a13d3f475c823079c3f0a20ee6957b9c
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-22 11:20:07 +0545

    test: inheritance parent class

---
+++ Inheritance/Parent.java
@@ -0,0 +1,14 @@
+package Inheritance;
+
+public class Parent {
+    
+    static void print(){
+        System.out.println("this is parent class");
+    }
+
+    public static void main(String[] args) {
+
+        Parent p = new Parent();
+        p.print();
+    }
+}
