237146f317ee06159b0b0a8a39fd929fb12fd691
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-22 13:15:19 +0545

    member detail

---
+++ Inheritance1/Member.java
@@ -0,0 +1,17 @@
+package Inheritance1;
+
+public class Member {
+    String Name;
+    int Age;
+    String phoneNumber;
+    String Address;
+    int Salary;
+
+    void printDetail(){
+        System.out.println("the name is : " + Name);
+        System.out.println("the age is : " + Age);
+        System.out.println("the phone number is : " + phoneNumber);
+        System.out.println("the address is : " + Address);
+        System.out.println("the salary is : " + Salary);
+    }
+}
