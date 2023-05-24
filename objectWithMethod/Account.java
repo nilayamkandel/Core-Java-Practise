c4f8eafc289ad8cde67eff27844c279eb25c7da6
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-24 23:02:55 +0545

    test: object with method

---
+++ objectWithMethod/Account.java
@@ -0,0 +1,39 @@
+package objectWithMethod;
+
+public class Account {
+    private int number;
+	private String type;
+	private int balance;
+	private String name;
+	private String bankName;
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBankName() {
+		return bankName;
+	}
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+}
