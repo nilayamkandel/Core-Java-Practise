d3d5b4c98581db17efdda27f2106460f794da57c
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-28 18:01:46 +0545

    main method

---
+++ Abstraction2/test.java
@@ -0,0 +1,12 @@
+package Abstraction2;
+
+public class test {
+
+    public static void main(String[] args) {
+        abstractClass a = new subClass();
+        a.abstractmethod();
+        a.nonAbstractMethod();
+    }
+
+    
+}
