58d105350a9d0e1e4358ce3204186dbac1abc086
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-28 17:03:05 +0545

    main method

---
+++ Abstraction1/test.java
@@ -0,0 +1,12 @@
+package Abstraction1;
+
+public class test {
+    
+    public static void main(String[] args) {
+        Animals a = new Cats();
+        a.cats();
+
+        Animals b = new Dogs();
+        b.dogs();
+    }
+}
