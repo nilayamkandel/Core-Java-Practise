b30c67a2450329afc1942d774ac9905c549c57bd
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-28 17:02:22 +0545

    sub class

---
+++ Abstraction1/Dogs.java
@@ -0,0 +1,14 @@
+package Abstraction1;
+
+public class Dogs extends Animals {
+    
+    @Override
+    void dogs() {
+        System.out.println("i bark");
+    }
+
+    @Override
+    void cats() {
+     
+    }
+}
