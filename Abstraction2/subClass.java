e00a85ad5f7590ed1d3be4d2dedad7b7f3c1ea91
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-28 18:01:15 +0545

    subclass

---
+++ Abstraction2/subClass.java
@@ -0,0 +1,10 @@
+package Abstraction2;
+
+public class subClass extends abstractClass{
+
+    @Override
+    void abstractmethod() {
+        System.out.println("this is abstract method");
+    }
+    
+}
