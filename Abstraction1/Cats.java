1f947ef354b893c3e21ec13bff35dd65f0f9eb68
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-28 17:01:47 +0545

    override

---
+++ Abstraction1/Cats.java
@@ -0,0 +1,17 @@
+package Abstraction1;
+
+public class Cats extends Animals{
+
+    @Override
+    void cats() {
+        System.out.println("i do meow");
+    }
+
+    @Override
+    void dogs() {
+        
+    }
+
+    
+    
+}
