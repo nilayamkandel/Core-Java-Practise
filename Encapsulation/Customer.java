5a1a9336768199ed7c3a741b2c2084e6431b8ee9
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-29 14:22:09 +0545

    pojo used, encapsulation

---
+++ Encapsulation/Customer.java
@@ -0,0 +1,48 @@
+package Encapsulation;
+
+public class Customer {
+    private int id;
+	private String name;
+	private String city;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	private int age;
+	private String phone;
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", city=" + city + ", age=" + age + ", phone=" + phone + "]";
+	}
+
+}
