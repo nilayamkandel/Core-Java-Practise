c3ff610d7a21fed4f548349d464f41caa2698b10
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-02 22:09:19 +0545

    recursion file

--- Recursion.java
+++ Recursion.java
@@ -1,18 +1,71 @@
 import java.util.Scanner;
 public class Recursion {
     //EXAMPLE - 1 [Print from 5-1]
-    public static void printNumb(int n) {
-        if(n==0){
+    // public static void printNumb(int n) {
+    //     if(n==0){
+    //         return;
+    //     }
+    //     //this helps to print the value
+    //     System.out.println(n);
+    //     //function have to call itself
+    //         printNumb(n-1); 
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+    //     printNumb(n);
+    // }
+
+
+   // EXAMPLE-2 [Print from 1-5]
+    public static void printNumb(int n){
+        if (n ==6){
             return;
         }
-        //this helps to print the value
         System.out.println(n);
-        //function have to call itself
-            printNumb(n-1); 
+        printNumb(n + 1);
+
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = 1;
         printNumb(n);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
