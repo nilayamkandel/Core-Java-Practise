16b8d40710cd2f319872179f1c693f2fd0456971
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-05 22:22:03 +0545

    write test

---
+++ fileHandling/WriteTest.java
@@ -0,0 +1,16 @@
+package fileHandling;
+import java.io.FileWriter;
+import java.io.IOException;
+public class WriteTest {
+    public static void main(String[] args) throws IOException {
+		FileWriter file = new FileWriter("c://product.txt",true);
+		
+		file.write("Name = tv\n");
+		file.write("Price = 500000\n");
+		file.write("Company = samsung\n");
+		file.write("Quantity = 800\n");
+		
+		file.close();
+		System.out.println("success");
+	}
+}
