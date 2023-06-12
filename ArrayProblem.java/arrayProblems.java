c463bf4629cd0885da3a779ba01890309dff41f0
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-12 21:21:24 +0545

    matrices

--- ArrayProblem.java/arrayProblems.java
+++ ArrayProblem.java/arrayProblems.java
@@ -170,6 +170,19 @@ public class arrayProblems {
 
                     
                    
+//  Create a Java program to add two matrices of size 2x3
+
+                int a[][] = {{3,2,3},{4,9,6}};
+                int b[][] = {{4,8,9},{7,11,12}};
+                int c[][] = new int[2][3];
+
+                for(int i = 0; i<2; i++){
+                    for(int j = 0; j<3; j++){
+                        c[i][j] = a[i][j] + b[i][j];
+                        System.out.print(c[i][j]+" ");    
+                    }    
+                    System.out.println(); 
+                }
 
                    
 
