282239adaf0c84be73f68833aaef4fe89b1edd85
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-13 22:17:33 +0545

    ten to power ten multiplication

---
+++ tenpowerten.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class tenpowerten {
+    
+	
+        public static void main(String arg[]) {
+            
+               long n,p,r=1;
+            
+               Scanner sc=new Scanner(System.in);
+               System.out.println("Enter number :");
+               n=sc.nextLong();
+    
+               System.out.println("Enter power :");
+               p=sc.nextLong();
+    
+               if(n>=0&&p==0)
+               {   
+                  r =1;
+               }
+               else if(n==0&&p>=1)
+            {   
+                    r=0;
+            } 
+               else
+            { 
+                      for(int i=1;i<=p;i++)
+                   { 
+                           r=r *n;
+                  }    
+               }    
+         
+                  System.out.println("Power of "+n+"^"+p+" = "+r);
+    
+            }
+    
+    }
+
