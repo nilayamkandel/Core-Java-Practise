7dfa84845ac48b5f9a946c2556b4bb775b9c448f
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-03 20:53:14 +0545

    practise

--- Constructors.java/ParameterizedConstructor.java
+++ Constructors.java/ParameterizedConstructor.java
@@ -2,15 +2,12 @@ public class ParameterizedConstructor {
     String userName;
     String password;
 
+    ParameterizedConstructor(){}
+
     ParameterizedConstructor(String userName, String password){
         // this two line mean i am calling instance variable via this.
         this.userName = userName;    
         this.password = password;
-        // this.result(); this statement mean i can call instance method via this
-        // this();        it means calling default constructor
-        // this("ram","8888") it means calling parameterised constructor
-        // object ra this vanya same nai ho except one difference i.e. obj cant call constructor
-        // but this can call constructor
     }
 
     void result(){
