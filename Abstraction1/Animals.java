49bd5f910410e5895ebc43ecbd887dd2f09250e7
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-04 22:03:41 +0545

    practise

--- Abstraction1/Animals.java
+++ Abstraction1/Animals.java
@@ -1,10 +1,5 @@
 package Abstraction1;
-/*
- * Create an abstract class 'Animals' with two abstract methods 'cats' and 'dogs'. 
- * Now create a class 'Cats' with a method 'cats' which prints "Cats meow" and a class
- *  'Dogs' with a method 'dogs' which prints "Dogs bark", both inheriting the class 'Animals'.
- *  Now create an object for each of the subclasses and call their respective methods.
- */
+
 public abstract class Animals {
     
     abstract void cats();
