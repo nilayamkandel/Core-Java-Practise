f98b5f11a876cb8f95f50e0fa39c73ba28d67e53
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-19 21:00:20 +0545

    practise

---
+++ practise/Area.java
@@ -0,0 +1,34 @@
+
+
+import java.util.Scanner;
+
+public class Area {
+	int l;
+	int b;
+
+	void setDim(int len, int bth) {		
+	}	
+	int getArea(){
+		int area = l * b;
+		System.out.println("The area of a rectangle is : " + area);
+		return area;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Area a1 = new Area();
+		System.out.println("Enter the size of length");
+		a1.l = sc.nextInt();
+		
+		System.out.println("Enter the size of breadth");
+		a1.b = sc.nextInt();
+		
+		a1.getArea();
+
+    }
+		
+		
+	
+
+}
