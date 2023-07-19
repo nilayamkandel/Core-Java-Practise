50dc01d6a7bbf0e2f8bbef1718cc20747829ee01
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-19 21:05:28 +0545

    practise

---
+++ practise/Rectangle.java
@@ -0,0 +1,21 @@
+public class Rectangle {
+    int l, b;
+	
+	Rectangle(int l, int b){
+		this.l = l;
+		this.b = b;
+	}
+	int Area() {
+		int r = l * b;
+		System.out.println("the area of rectangle is :" + r);
+		return r;
+	}
+	
+	public static void main(String[] args) {
+		Rectangle r1 = new Rectangle(4,5);
+		r1.Area();
+		
+		Rectangle r2 = new Rectangle(5,8);
+		r2.Area();
+	}
+}
