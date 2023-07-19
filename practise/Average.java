65ad1fc19d8335b78c6f8d7d53b97e8a64c3bdf4
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-19 21:01:06 +0545

    practise

---
+++ practise/Average.java
@@ -0,0 +1,17 @@
+public class Average {
+    int a = 1;
+	int b = 2;
+	int c = 3;
+	
+	void Calculate() {
+		int sum = a+b+c;
+		System.out.println("the sum is : " + sum);
+		int avg = sum/3;
+		System.out.println("the avg is : " +avg);
+	}
+	
+	public static void main(String[] args) {
+		Average a = new Average();
+		a.Calculate();
+	}
+}
