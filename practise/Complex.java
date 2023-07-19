e469194cdab7a983bb2d33f29d1c4f7cc6d42005
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-19 21:02:42 +0545

    complex data

---
+++ practise/Complex.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Complex {
+    int x, y;
+	void sum() {
+		int add = x + y;
+		System.out.println("the sum is : " + add);
+	}
+	void diff() {
+		int difference = x - y;
+		System.out.println("the diff is : " + difference);
+	}
+	void product() {
+		int pro = x * y;
+		System.out.println("the sum is : " + pro);
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Complex c = new Complex();
+		System.out.println("enter the numbers");
+		c.x = sc.nextInt();
+		c.y = sc.nextInt();
+		c.sum();
+		
+		
+	}
+}
