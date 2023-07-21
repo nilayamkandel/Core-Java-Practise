67e5a0e5a6f0e8edf873407c053bea187e359b38
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-21 22:28:30 +0545

    practise

--- ArrayProblem.java/arrayProblems.java
+++ ArrayProblem.java/arrayProblems.java
@@ -27,24 +27,24 @@ public class arrayProblems {
 // Write a program to find out whether a given integer is present in an array or not
             // this can also be written as int findNum[] = {1,12,13,14}
 
-            // int findNum[] = new int[5];
-            // findNum[0] = 1;
-            // findNum[1] = 13;
-            // findNum[2] = 12;
-            // findNum[3] = 14;
-            // int number;
-            // Scanner sc = new Scanner(System.in);
-            // System.out.println("enter the integer you are searching");
-            // number = sc.nextInt();
-            // for(int x: findNum){
-            //     if(x == number){
-            //         System.out.println("number available");
-            //         return;
-            //     }else{
-            //         System.out.println("not found");
-            //         return;
-            //     }
-            // }
+            int findNum[] = new int[5];
+            findNum[0] = 1;
+            findNum[1] = 13;
+            findNum[2] = 12;
+            findNum[3] = 14;
+            int number;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter the integer you are searching");
+            number = sc.nextInt();
+            for(int x: findNum){
+                if(x == number){
+                    System.out.println("number available");
+                    return;
+                }else{
+                    System.out.println("not found");
+                    return;
+                }
+            }
 
 
 
@@ -186,9 +186,9 @@ public class arrayProblems {
 
 
 // Write a Java program to find whether an array is sorted or not
-                    int data[] = {3,6,8,3,8,1,9,3};
-                    Arrays.sort(data);
-                    System.out.println(Arrays.toString(data));
+                    // int data[] = {3,6,8,3,8,1,9,3};
+                    // Arrays.sort(data);
+                    // System.out.println(Arrays.toString(data));
                    
 
                     
--- Polymorphism/polygon.java
+++ Polymorphism/polygon.java
@@ -3,21 +3,19 @@ package Polymorphism;
 // polymorphism with method overloading.
 // 1. changing data type name
 // 2. adding more parameter 
-public class polygon {
-            
+public class polygon {           
     void Square(int a) {
         System.out.println(" the value of a is : " + a);       
       }
       
-    void Square(String a) {
-        System.out.println(" the value of a is : " + a);       
+    void Square(int a , int b) {
+        System.out.println(" the value of a is : " + (a+b));       
       }
-      
-     
+        
 public static void main(String[] args) {
           polygon p = new polygon();
           p.Square(1);
-          p.Square("1");
+          p.Square(2,5);
         }
 }
 
