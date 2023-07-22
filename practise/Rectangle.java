e3594958a36ad8203d0c798e921d55f63e209241
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-22 21:52:43 +0545

    method practise

--- practise/Rectangle.java
+++ practise/Rectangle.java
@@ -12,7 +12,7 @@ public class Rectangle {
 	}
 	
 	public static void main(String[] args) {
-		Rectangle r1 = new Rectangle(4,5);
+		Rectangle r1 = new Rectangle(5,5);
 		r1.Area();
 		
 		Rectangle r2 = new Rectangle(5,8);
