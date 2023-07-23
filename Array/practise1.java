5862ba3c9d4a1fbb1b911ca814494c50472f46a1
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-23 21:40:34 +0545

    find the length of values

---
+++ Array/practise1.java
@@ -0,0 +1,9 @@
+package Array;
+// finding the length of the values
+public class practise1 {
+    
+    public static void main(String[] args) {
+        String city[] =  {"ktm","lalitpur","bkt"};
+        System.out.println("the number of cities are: " + city.length);
+    }
+}
