48b70a1608ada8ccdf84d799dc4b4effe5894410
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-23 21:42:57 +0545

    using index to find value

---
+++ Array/practise2.java
@@ -0,0 +1,8 @@
+package Array;
+
+public class practise2 {
+    public static void main(String[] args) {
+        int num[] = {1,2,3,4,56,7};
+        System.out.println("the number at 2nd is :" + num[1]);
+    }
+}
