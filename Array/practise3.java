f7f1d7a69a25499d0d0624cc51c63a84f92977bc
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-23 21:45:23 +0545

    exchange numbers

---
+++ Array/practise3.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class practise3 {
+    public static void main(String[] args) {
+        int num[] = {1,2,3,4,5,6,7};
+        num[0] = 2;
+        System.out.println("the replaced numb is:"+ num[0]);
+
+    }
+}
