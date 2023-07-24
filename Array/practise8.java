d2a1e065fb32868b4ea2b15291225ed8bba77baf
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-24 09:57:29 +0545

    alternative method to  find data

--- Array/practise8.java
+++ Array/practise8.java
@@ -4,27 +4,53 @@ import java.util.Scanner;
 
 //Write a Java program to test if an array contains a specific value.
 public class practise8 {
-    public static void main(String[] args) {
-        System.out.println("enter the number you are searching");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        boolean found = false;
 
-        int num[] = {2,3,4,5,6,7,8};
-        for(int x: num){
-            if(n ==x){
-                found = true;
+    //trick 1 to solve
+
+    // public static void main(String[] args) {
+    //     System.out.println("enter the number you are searching");
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+    //     boolean found = false;
+
+    //     int num[] = {2,3,4,5,6,7,8};
+    //     for(int x: num){
+    //         if(n ==x){
+    //             found = true;
                  
-                break;
+    //             break;
                    
+    //         }
+    //     }
+    //     if(found){
+    //         System.out.println(n + " it exists" );
+    //     }
+    //     else{
+    //         System.out.println(n + " doesn't exist" );
+    //     }
+    // }
+
+    //trick 2 to solve
+        static boolean findNum(int[] num,int item){
+            for(int x: num){
+                if(x==item){
+                    return true;
+                }
             }
+            return false;
         }
-        if(found){
-            System.out.println(n + " it exists" );
-        }
-        else{
-            System.out.println(n + " doesn't exist" );
-        }
+
+    public static void main(String[] args) {
+        int[] num = {1,2,3,4,5,6,7,8};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number you want to search");
+        int item = sc.nextInt();
+        System.out.println(findNum(num,item));
+
+        
+
+
+       
     }
     
 }
