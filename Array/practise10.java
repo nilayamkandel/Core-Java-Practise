0546ade1f7ffbba79c39c2a8aca320c566453c47
neelayam kandel <neelayamdottkandel@gmail.com>
2023-08-05 19:53:15 +0545

    practise

--- Array/practise10.java
+++ Array/practise10.java
@@ -1,5 +1,19 @@
 package Array;
-// Write a Java program to calculate the average value of array elements.
-public class practise10 {
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+// Write a Java program to remove a specific element from an array.
+public class practise10 {
+    public static void main(String[] args) {
+        int[] num = {1,2,3,4,5,6};
+        System.out.println("the original array is:" + Arrays.toString(num));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number you want to remove");
+        int n = sc.nextInt();
+        int removeIndex = n;
+        
+        
+
+    }
 }
