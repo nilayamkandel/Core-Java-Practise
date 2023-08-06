fdd9e548a334ca01c9585a4b6f7e15e3bb55f9c5
neelayam kandel <neelayamdottkandel@gmail.com>
2023-08-06 21:33:22 +0545

    practise

--- DiamondPattern.java
+++ DiamondPattern.java
@@ -32,6 +32,7 @@ import java.util.*;
                     System.out.print("*");
                 }
                 System.out.println("");
+                System.out.println("-----------------------");
             }
             
             
