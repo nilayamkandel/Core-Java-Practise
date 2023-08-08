bc29cbe93015e01a47df3e32a68088c93bf7ec8c
neelayam kandel <neelayamdottkandel@gmail.com>
2023-08-08 21:48:19 +0545

    practise

--- practise/geometry.java
+++ practise/geometry.java
@@ -2,6 +2,7 @@ public class geometry {
     void area(int l, int b) {
         int a = l*b;
         System.out.println("the value of a is :" + a);
+        System.out.println("-----------");
     }
     void area(int s) {
         int b = s*s*s;
