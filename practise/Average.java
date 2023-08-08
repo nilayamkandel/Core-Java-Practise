2f4d5c7793ec0e53d9d9f4452552f304fdc697e8
neelayam kandel <neelayamdottkandel@gmail.com>
2023-08-08 21:47:47 +0545

    practise

--- practise/Average.java
+++ practise/Average.java
@@ -7,9 +7,12 @@ public class Average {
 		int sum = a+b+c;
 		System.out.println("the sum is : " + sum);
 		int avg = sum/3;
+		System.out.println("==================");
 		System.out.println("the average is : " +avg);
 		System.out.println("-------------");
 	}
+
+
 	
 	public static void main(String[] args) {
 		Average a = new Average();
