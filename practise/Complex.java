0e288e121dc91cd345bf7e566a6761c3225f1bfe
neelayam kandel <neelayamdottkandel@gmail.com>
2023-08-08 21:45:15 +0545

    practise

--- practise/Complex.java
+++ practise/Complex.java
@@ -12,7 +12,7 @@ public class Complex {
 	}
 	void product() {
 		int pro = x * y;
-		System.out.println("the sum is : " + pro);
+		System.out.println("the sum are: " + pro);
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
