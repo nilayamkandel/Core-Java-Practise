33117e9f10d9041e9eb78fd5c2c23d1e67c93321
neelayam kandel <neelayamdottkandel@gmail.com>
2023-08-12 22:20:56 +0545

    practise

--- Interface/test.java
+++ Interface/test.java
@@ -18,5 +18,6 @@ public class test {
         System.out.println("9999999999999990");
         System.out.println("...........");
         System.out.println("---------");
+        System.out.println("");
     }
 }
