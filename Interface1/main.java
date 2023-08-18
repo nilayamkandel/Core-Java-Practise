8c5e532c05a9cd97afa6bdac8c24bdc12bdc048e
neelayam kandel <neelayamdottkandel@gmail.com>
2023-08-18 19:56:46 +0545

    print

--- Interface1/main.java
+++ Interface1/main.java
@@ -30,5 +30,6 @@ public class main {
         System.out.println("-----------");
         System.out.println(".........");
         System.out.println(";;;;;;;;");
+        System.out.println(".........");
     }
 }
