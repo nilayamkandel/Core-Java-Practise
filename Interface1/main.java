e3c3a5b135c50af502d7e0cbbac4d4f0ca973bbb
neelayam kandel <neelayamdottkandel@gmail.com>
2023-09-05 17:29:58 +0545

    prctise

--- Interface1/main.java
+++ Interface1/main.java
@@ -89,6 +89,7 @@ public class main {
         System.out.println(".......");
         System.out.println(".........");
         System.out.println("........");
+        System.out.println("");
     
     }
 }
