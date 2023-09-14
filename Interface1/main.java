ff8bf27955f763e1ff3a9593fdc5702d6f4ca825
neelayam kandel <neelayamdottkandel@gmail.com>
2023-09-14 22:30:42 +0545

    practise

--- Interface1/main.java
+++ Interface1/main.java
@@ -108,6 +108,7 @@ public class main {
         System.out.println("........");
         System.out.println("..........");
         System.out.println("..........");
+        System.out.println("..............");
     
     }
 }
